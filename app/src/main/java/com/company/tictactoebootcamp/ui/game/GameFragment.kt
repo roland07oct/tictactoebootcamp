5ca8633356a1d2dc5186538647f327394b9e5892
roland07oct <rolandsimionas@gmail.com>
2024-09-20 14:30:32 +0300

    delete unused imports

--- app/src/main/java/com/company/tictactoebootcamp/ui/game/GameFragment.kt
+++ app/src/main/java/com/company/tictactoebootcamp/ui/game/GameFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import com.company.tictactoebootcamp.ScoreItem
 import com.company.tictactoebootcamp.data.model.Board
 import com.company.tictactoebootcamp.data.model.BoardState
